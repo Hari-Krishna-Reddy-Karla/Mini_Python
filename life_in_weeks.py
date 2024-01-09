d77679ef969c95c476e36bf57b87b48d3808ea31
Hari-Krishna-Reddy-Karla <131398907+Hari-Krishna-Reddy-Karla@users.noreply.github.com>
2024-01-09 16:19:35 -0600

    Added a program to calculate how many weeks we got

---
+++ life_in_weeks.py
@@ -0,0 +1,5 @@
+"""This program calculates how many weeks are left in one's life according to average life span."""
+
+age = input("How old are you in years?")
+years = 75 - int(age)
+print(f"You have {years*52} weeks left.")

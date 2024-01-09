75c52e777ceb2b232a305c4655120ed645c1e447
Hari-Krishna-Reddy-Karla <131398907+Hari-Krishna-Reddy-Karla@users.noreply.github.com>
2024-01-09 16:35:00 -0600

    adding a program that can calculate tips

---
+++ tip_calculator.py
@@ -0,0 +1,11 @@
+"""This program Helps in calculating tips"""
+
+print("Welcome to the Tip Calculator")
+total_bill = float(input("What was the total bill?\n"))
+tip_percent = float(input("What percentage tip would you like to give?\n"))
+no_of_people = int(input("How many people to split the bill?\n"))
+
+tip = total_bill * tip_percent / 100
+bill_each = (total_bill + tip) / no_of_people
+
+print(f'Each person should pay: {round(bill_each, 2)}')

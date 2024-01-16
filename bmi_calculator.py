fae3ade2d96d4440063df8bac525531658bb3f09
Hari-Krishna-Reddy-Karla <131398907+Hari-Krishna-Reddy-Karla@users.noreply.github.com>
2024-01-16 16:12:10 -0600

    Enhanced bmi_calculator.py to classify weights

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -2,9 +2,20 @@
 
 
 # 1st input: enter height in meters e.g: 1.65
-height = input()
+height = float(input("Please enter your height in metres.\n"))
 # 2nd input: enter weight in kilograms e.g: 72
-weight = input()
+weight = float(input("Please enter your weight in Kilograms.\n"))
 
 # Below equation is the formula for calculating the bmi (w/h*h)
-print(int(float(weight)/float(height) ** 2))
+bmi = weight/ (height ** 2)
+
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:  
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")

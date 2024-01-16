afda7d875c7fb54ab6a646d4ac3dcad4a23cb98d
Hari-Krishna-Reddy-Karla <131398907+Hari-Krishna-Reddy-Karla@users.noreply.github.com>
2024-01-16 16:44:14 -0600

    added photo feature to roller_coaster.py

--- roller_coaster.py
+++ roller_coaster.py
@@ -2,16 +2,23 @@
 
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm?\n"))
-
+bill = 0
 if height > 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age?\n"))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
     else:
-        print("Please pay $12.")
+        bill = 12
+        
+    wants_photo = input("Would you like your picture taken? Y or N.\n")
+    if wants_photo == 'Y' or wants_photo == 'y':
+        bill += 3
+    print(f"Please pay ${bill}.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
+    
+
     
